c00f0b240b50ab64780d7a5e55c6d463768ef3ba
SafeeUrRehman65 <safee_rehman2001@hotmail.com>
2026-07-28 12:24:42 +0500

    Clean reset git repo

--- backend/evaluations/single-step/node1/evaluator.py
+++
@@ -1,5 +0,0 @@
-# Evaluator
-
-def correct_classification(outputs: dict, reference_outputs: dict) -> bool:
-    """Check if the agent chose the correct route."""
-    return outputs["classification"] == reference_outputs["classification"]
--- backend/models/chat_title_description.py
+++
@@ -1,5 +0,0 @@
-
-from pydantic import BaseModel, Field
-class LLMTitleSchema(BaseModel):
-    title: str = Field("New Chat", description="A short title of max 6 words for a chat conversation")
-    description: str = Field("Chat Description", description = "A single short sentence description of a chat conversation")
